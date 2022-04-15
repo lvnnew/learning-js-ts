32f0fcdccdf9180f7b23a141fa06e9b4b2b6c852
владимир Лещенко <pirat_2-5@mail.ru>
2022-04-15 22:31:26 +0300

    #6 - Добавил файлы anonymousFunctionsTask1.ts, anonymousFunctionsTask2.ts, anonymousFunctionsTask3.ts

---
+++ src/anonymousFunctionsTask3.ts
@@ -0,0 +1,11 @@
+/* eslint-disable no-console */
+/* eslint-disable eol-last */
+export {};
+
+// yarn ts-node src/anonymousFunctionsTask3.ts
+
+const printStr = (arg: string) => {
+  console.log(arg, 'Но это не точно');
+};
+
+printStr('Параметр');
