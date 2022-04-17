9b08d0131752d625f681960d5c00f85bea3dc1d8
владимир Лещенко <pirat_2-5@mail.ru>
2022-04-17 10:20:55 +0300

    Изменил сonditionalStatementsTask2.ts

--- src/сonditionalStatementsTask2.ts
+++ src/сonditionalStatementsTask2.ts
@@ -3,12 +3,12 @@ import {log} from './log';
 
 // yarn ts-node src/сonditionalStatementsTask2.ts
 
-let first = 0;
+let first = 5;
 let val1 = 20;
 let val2 = 10;
 
 const app = () => {
-  if (first === 20) {
+  if (first === 5) {
     log.info(first + val1 + val2);
   } else if (first === 0 || -5) {
     log.info(val1 - val2);
