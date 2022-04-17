a82a9af503977c81fa9261af2ef903cba3db6fd9
владимир Лещенко <pirat_2-5@mail.ru>
2022-04-17 10:17:00 +0300

    Изменил anonymousFunctionsTask3.ts

--- src/anonymousFunctionsTask3.ts
+++ src/anonymousFunctionsTask3.ts
@@ -3,9 +3,16 @@
 export {};
 
 // yarn ts-node src/anonymousFunctionsTask3.ts
-
+// В данном случае значение параметра задано как по умолчанию
 const printStr = (arg = 'Параметр') => {
   console.log(arg, 'Но это не точно');
 };
 
 printStr();
+
+// Вариант с передачей параметра в моменте вызове функции
+const printString = (arg: string) => {
+  console.log(arg, 'Но это не точно');
+};
+
+printString('Параметр');
