e26fdb617fe87dcb9360794e8386277e97508c3b
владимир Лещенко <pirat_2-5@mail.ru>
2022-04-21 15:54:52 +0300

    Исправил файл сonditionalStatementsTask2.ts

--- src/сonditionalStatementsTask2.ts
+++ src/сonditionalStatementsTask2.ts
@@ -8,9 +8,9 @@ let val1 = 20;
 let val2 = 10;
 
 const app = () => {
-  if (first === 5) {
+  if (first > 0) {
     log.info(first + val1 + val2);
-  } else if (first === 0 || -5) {
+  } else if (first <= 0) {
     log.info(val1 - val2);
   }
 };
